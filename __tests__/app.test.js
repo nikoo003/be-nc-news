48f8299343b42082622226cb9414f90eda3097e3
Nikoo Farsi <nikoo@nikoos-mbp.home>
2024-05-28 14:25:08 +0100

    Set up /api/topics endpoint and initial testing suite, covering all expected responses and error scenarios

---
+++ __tests__/app.test.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const db = require("../db/connection");
+
+afterAll(() => {
+  return db.end();
+});
+beforeEach(() => {
+  console.log("seeding");
+  return seed(data);
+});
+
+describe.only("GET api/topics", () => {
+  test("GET:200 sends an array of topics to the client with the correct array length", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.topics).toHaveLength(3);
+      });
+  });
+  test("Each topic object has correct structure", () => {
+    return request(app)
+      .get("/api/topics")
+      .then(({ body }) => {
+        body.topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
+  test.only("GET:404 responds with an error message when accessing a nonexistent route", () => {
+    return request(app)
+      .get("/api/notARoute")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
+      });
+  });
+});
