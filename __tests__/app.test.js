073a96a3e1be98dbb9d5ad83763ea8c35ccb0b3a
Nikoo Farsi <nikoo@nikoos-mbp.home>
2024-05-29 16:25:27 +0100

    Refactured test suit to increase readabilty

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,30 +55,31 @@ describe("GET /api/articles", () => {
             created_at: expect.any(String),
             votes: expect.any(Number),
             article_img_url: expect.any(String),
-            comment_count: expect.any(String)
+            comment_count: expect.any(String),
           });
         });
         expect(body.articles).toBeSortedBy("created_at", { descending: true });
       });
   });
+});
 
-  describe("GET /api/articles/:article_id", () => {
-    test("200: sends an object to the client with the correct object structure", () => {
-      return request(app)
-        .get("/api/articles/3")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toMatchObject({
-            title: expect.any(String),
-            topic: expect.any(String),
-            author: expect.any(String),
-            body: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
-          });
+describe("GET /api/articles/:article_id", () => {
+  test("200: sends an object to the client with the correct object structure", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toMatchObject({
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          body: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
         });
-    });
+      });
+  });
   test("400: responds with an error when article_id is invalid", () => {
     return request(app)
       .get("/api/articles/invalid_id")
