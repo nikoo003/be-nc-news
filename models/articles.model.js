fc1ccb493828be121cc156425e59952a52dbba0a
Nikoo Farsi <nikoo@nikoos-mbp.home>
2024-05-29 15:58:38 +0100

    Adds query for selecting articles, including comment count, sorted by creation date

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,7 +1,19 @@
 const db = require("../db/connection");
 
 exports.selectArticleById = (article_id) => {
-    return db.query("SELECT * FROM articles WHERE article_id = $1", [article_id]).then((result)=>{
+    return db.query("SELECT * FROM articles WHERE article_id = $1;", [article_id]).then((result)=>{
         return result.rows[0]
     })
 };
+
+exports.selectArticles = () => {
+    return db.query(`
+      SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count 
+      FROM articles 
+      LEFT JOIN comments ON comments.article_id = articles.article_id
+      GROUP BY articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url
+      ORDER BY articles.created_at DESC;
+    `).then(({ rows }) => {
+      return rows;
+    });
+  };
